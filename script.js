4520276faf5897308709126ed890a75621f22884
FilipyJ <josephfilipy@gmail.com>
2021-08-02 20:52:28 -0300

    Criação do DOM JavaScript com HTML

---
+++ script.js
@@ -0,0 +1,16 @@
+// O aplicativo constituirá em uma simulação  dada uma velocidade máxima permitida na via de "60 km " p/hr na qual irá verificar a velocidade do carro e determinar se está apto a andar na pista ou não 
+
+// A forma na qual ele vai ser verificiadco é recebendo a velocidade pelo "input" e se, "input " for maior que "60km/hr,irá aparecer uma mensagem dizendo a velocidade atual que ele está no momento e que não pode andar na via, se tiver próximo dos 60, avisar a velocidade atual e dizer que está próximo ao limite 
+
+//futuramente vou fazer um upgrade desse sistema e deixar ele mais dinâmico, fazendo um botão para dar uma velocidade randomica para o veiculo e apartir da velocidade o script continuar normalmente o seu ciclo 
+
+
+var velocidadeMax = 60
+
+
+
+function sistema(){
+   var velocidade = window.document.getElementById('velocidade').value
+
+   console.log(velocidade)
+}
